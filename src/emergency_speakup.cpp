d116da9102f85b63e7186d0501c5a32f5db09131
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-07-10 06:57:26 +0000

    why wasnt this on the svn
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_perception/amigo_emergency_speakup@5192 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ src/emergency_speakup.cpp
@@ -0,0 +1,51 @@
+#include "ros/ros.h"
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+
+using namespace std;
+
+ros::Time time_init, time_current;
+ros::Publisher pub;
+ros::Subscriber sub;
+bool alarm_state = false;
+std_msgs::String speech_msg;
+bool button_state = false;
+
+
+void emergencyCallback(const std_msgs::BoolPtr&  msg)
+{
+	if (msg->data && !button_state) 
+	{
+		speech_msg.data = string("Emergency button pressed");
+		pub.publish(speech_msg);
+		button_state = true;
+	} 
+	else if (!msg->data && button_state) 
+	{
+		speech_msg.data = string("Emergency button released");
+		pub.publish(speech_msg);
+		button_state = false;
+	}
+	
+	
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "emergency_speakup");
+	ros::NodeHandle n;
+	
+	sub = n.subscribe("/emergency_switch", 1, emergencyCallback);
+	pub = n.advertise<std_msgs::String>("/amigo_speak_up", 50);
+	
+	time_init = ros::Time::now();	
+	
+	ros::Rate loop_rate(1.0);
+
+	while(n.ok())
+	{
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
+}
